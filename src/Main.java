84020a882beebe72c5e0e14340c371bdc6272f92
cinthynhac <cinthya.allvez@gmail.com>
2025-11-17 21:25:19 -0300

    Criação de Classe produto e adicionando algumas informações na main;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        User usuario = new User();
+
+        int menu1;
+
+        //Menu Interativo 1:
+        System.out.println("Bem vindo ao seu sistema de vendas integrado! Escolha uma das opções abaixo: ");
 
 
     }
---
+++ src/Produto.java
@@ -0,0 +1,7 @@
+public class Produto {
+    private String nomedoproduto;
+    private double preço;
+    private String descricaodoproduto;
+
+
+}
